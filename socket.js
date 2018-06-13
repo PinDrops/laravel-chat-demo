e53928546ad662c437660d84a546ad4b15422755
Gareth Pursehouse <gpursehouse@internetbrands.com>
2018-06-13 15:12:03 -0700

    get all channels

--- socket.js
+++ socket.js
@@ -25,24 +25,28 @@ var io      = require('socket.io')(server);
 var Redis = require('ioredis');
 var redis = new Redis();
 
-redis.subscribe('test-channel', function(err, count) {});
+//redis.subscribe('test-channel', function(err, count) {});
+//
+//redis.subscribe('room',         function(err, count) {});
+//redis.subscribe('direct',       function(err, count) {});
+//redis.subscribe('alert',        function(err, count) {});
 
-redis.subscribe('room',         function(err, count) {});
-redis.subscribe('direct',       function(err, count) {});
-redis.subscribe('alert',        function(err, count) {});
-
-//io.on('connection', function(socket) {});
-//redis.psubscribe('*', function(err, count) {});
-//redis.on('pmessage', function(subscribed, channel, message) {
-//    message = JSON.parse(message);
-//    io.emit(channel + ':' + message.event, message.data);
-//});
-
-redis.on('message', function(channel, message) {
-    console.log('Message Recieved: ' + message);
+io.on('connection', function(socket) {});
+redis.psubscribe('*', function(err, count) {
+//    console.log('subscribed : ' + count);
+//    console.log(err);
+});
+redis.on('pmessage', function(subscribed, channel, message) {
+    console.log( 'subscribed ' + subscribed + ' : Channel ' + channel );
     message = JSON.parse(message);
     io.emit(channel + ':' + message.event, message.data);
 });
+
+//redis.on('message', function(channel, message) {
+//    console.log('Message Recieved: ' + message);
+//    message = JSON.parse(message);
+//    io.emit(channel + ':' + message.event, message.data);
+//});
 
 server.listen(3000, function(){
     console.log('Listening on Port 3000');
